30634003164cb3cef31c1fa7ccbef3387b035cf9
0x876 <marecages.mastique.0x@icloud.com>
2024-02-12 14:24:12 +0100

    Favicon add

--- js/load.js
+++ js/load.js
@@ -1,4 +1,1 @@
-manga_list = fetch('/manga-list.json').then((response) => response.json());
-
-
-console.log(manga_list)
+manga_list=fetch("/manga-list.json").then((response=>response.json()));console.log(manga_list);
--- start.sh
+++
@@ -1,4 +0,0 @@
-while true
-do
-    ssh -R s-scan.serveo.net:80:localhost:8080 serveo.net
-done
